ffc9644d686fa8bea738a85e910e0d2b1ac8b093
chrisaydat <chrisaydat@gmail.com>
2024-02-09 01:48:29 +0000

    stripeflutter

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_stripe/flutter_stripe.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:stripewithflutter/screens/home_screen.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  Stripe.publishableKey =
+      'pk_live_51JeKjGKqLaeFV3jBVGQpnFDsKKamd5WscXhH1kDIzoXFv61ZYfPbYfb7t93kcKDJPFE1eD7PinngRCVR5gcFfwjS00Xhy9evkF';
+  // await dotenv.load(fileName = "assets/.env");
+
   runApp(const MainApp());
 }
 
--- lib/screens/card_form_screen.dart
+++ lib/screens/card_form_screen.dart
@@ -1,16 +1,16 @@
 import 'package:flutter/material.dart';
 
-class CardFromScreen extends StatelessWidget{
+class CardFromScreen extends StatelessWidget {
   const CardFromScreen({super.key});
 
-@override
-Widget build(BuildContext context) {
-  return Scaffold(
-    backgroundColor: Colors.white,
-    appBar: AppBar(
-      title: const Text('Pay with Credit Card'),
-      backgroundColor: Colors.pink,
-    ),
-  );
-}
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        title: const Text('Pay with Credit Card'),
+        backgroundColor: Colors.pink,
+      ),
+    );
+  }
 }
