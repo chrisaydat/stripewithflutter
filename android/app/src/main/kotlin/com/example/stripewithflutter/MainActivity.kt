e0bb467915898b6c5321bc90a6faf638d78fde8b
Chris Festus Otopa Ayeh-Datey <chrisaydat@gmail.com>
2024-04-19 12:35:48 +0000

    podfile updated from 12 to 13 changed main activity from flutteractivity to flutterfragmentactivity as recommended by the stripeflutter repo

--- android/app/src/main/kotlin/com/example/stripewithflutter/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/stripewithflutter/MainActivity.kt
@@ -2,5 +2,5 @@ package com.example.stripewithflutter
 
 import io.flutter.embedding.android.FlutterActivity
 
-class MainActivity: FlutterActivity() {
+class MainActivity: FlutterFragmentActivity() {
 }
